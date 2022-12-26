aa1ff47ed3f730204f029ce895f93f91b69e9284
Evgen Goravski <evgoravsky@gmail.com>
2022-12-26 16:10:05 +0300

    Added emoji

--- src/main/java/org/goravski/exchangeCurrencyBelBot/entity/BanksType.java
+++ src/main/java/org/goravski/exchangeCurrencyBelBot/entity/BanksType.java
@@ -2,13 +2,14 @@ package org.goravski.exchangeCurrencyBelBot.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.goravski.exchangeCurrencyBelBot.util.Emoji;
 
 @Getter
 @AllArgsConstructor
 public enum BanksType {
     SBERBANK ("СБЕРБАНК"),
     BELARUSBANK("БЕЛАРУСБАНК"),
-    BACK("НАЗАД");
+    BACK("НАЗАД  " + Emoji.TURN);
 
     private String nameBank;
 
