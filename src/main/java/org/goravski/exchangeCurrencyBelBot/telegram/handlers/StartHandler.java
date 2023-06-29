a5c92cfd398928d629c83bd1a847a99912ff2133
Evgen Goravski <evgoravsky@gmail.com>
2023-06-29 17:27:42 +0300

    Fix: move 100-banner2_C2.png to resource directory

--- src/main/java/org/goravski/exchangeCurrencyBelBot/telegram/handlers/StartHandler.java
+++ src/main/java/org/goravski/exchangeCurrencyBelBot/telegram/handlers/StartHandler.java
@@ -33,7 +33,7 @@ public class StartHandler extends AbstractMessageHandler {
         return SendPhoto.builder()
                 .chatId(chatId)
                 .photo(new InputFile(
-                        new File("assests\\100-banner2_C2.png")))
+                        new File("100-banner2_C2.png")))
                 .caption("Привет, " + name + "!\n"
                         + "Здесь можно:\n"
                         + "1. Посчитать результат обмена валют для выбранного банка.\n"
