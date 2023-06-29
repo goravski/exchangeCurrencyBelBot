a62dde12f957a12ed12e3d5ac39f03f964e345c9
Evgen Goravski <evgoravsky@gmail.com>
2023-06-29 20:51:38 +0300

    Fix:  path src/

--- src/main/java/org/goravski/exchangeCurrencyBelBot/telegram/handlers/StartHandler.java
+++ src/main/java/org/goravski/exchangeCurrencyBelBot/telegram/handlers/StartHandler.java
@@ -33,7 +33,7 @@ public class StartHandler extends AbstractMessageHandler {
         return SendPhoto.builder()
                 .chatId(chatId)
                 .photo(new InputFile(
-                        new File("")))
+                        new File("src/main/resources/assets/100-banner2_C2.png")))
                 .caption("Привет, " + name + "!\n"
                         + "Здесь можно:\n"
                         + "1. Посчитать результат обмена валют для выбранного банка.\n"
