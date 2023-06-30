e979a8419b43634e419eabf92d0bcb15f101c966
Evgen Goravski <evgoravsky@gmail.com>
2023-06-30 10:15:50 +0300

    Fix:   check path

--- src/main/java/org/goravski/exchangeCurrencyBelBot/ExchangeCurrencyBelBotApplication.java
+++ src/main/java/org/goravski/exchangeCurrencyBelBot/ExchangeCurrencyBelBotApplication.java
@@ -4,12 +4,16 @@ package org.goravski.exchangeCurrencyBelBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class ExchangeCurrencyBelBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeCurrencyBelBotApplication.class, args);
-
+		File dir = new File(".");
+		Stream.of(dir.listFiles()).forEach(System.out ::println);
 	}
 
 }
